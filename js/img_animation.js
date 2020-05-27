3f2799d7196d40e1bf5738584242c85d9cc28c4f
Stefano <stefanolagattolla.s@gmail.com>
2020-05-27 23:24:15 +0200

    first commit, deploy de website

---
+++ js/img_animation.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+
+
+
+<script>
+        ScrollOut({
+    once: true
+    });
+    function scrollToElements(){
+        window.scrollTo({
+            top: 800,
+            behavior: 'smooth'
+        });
+    }
+</script>
+
